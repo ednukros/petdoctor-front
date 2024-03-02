159c9029b283cb913d1f584c65523b4021ba2689
ednukros <isanmenarguez@gmail.com>
2024-03-02 19:09:46 +0100

    Feat:footer and chat button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ChatComponent } from './shared/chat/chat.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    FooterComponent,
+    DashboardComponent,
+    ChatComponent
   ],
   imports: [
     BrowserModule,
